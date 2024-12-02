8a48ceaa65a388c358dfe02a84b75f5098783669
Plictox <tariot.louis@gmail.com>
2024-12-02 16:52:54 +0100

    new files

--- GazeML-master/GazeML-master/src/models/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Model definitions (one class per file) to define NN architectures."""
-from .elg import ELG
-from .dpg import DPG
-
-__all__ = ('ELG', 'DPG')
--- opengaze-master/opengaze/download_models.sh
+++
@@ -1,9 +0,0 @@
-OPENGAZE_DIR=~/OpenGaze
-
-mkdir -p $OPENGAZE_DIR/content/caffeModel
-
-cd $OPENGAZE_DIR/content/caffeModel
-
-wget https://datasets.d2.mpi-inf.mpg.de/MPIIGaze/alexnet_face.prototxt
-
-wget https://datasets.d2.mpi-inf.mpg.de/MPIIGaze/alexnet_face.caffemodel
