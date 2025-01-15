cb950de5b2f4eac53d81e15b11a6a6aec70f85f9
NathanN74 <nathnoel74@hotmail.fr>
2025-01-15 14:07:16 +0100

    ajout de tout

---
+++ EyeTrackingModels/opengaze/caffe-layers/src/include/caffe/layers/dspp_layer.hpp
@@ -0,0 +1,50 @@
+#ifndef CAFFE_DSPP_LAYER_HPP_
+#define CAFFE_DSPP_LAYER_HPP_
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/layers/data_layer.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/layers/loss_layer.hpp"
+#include "caffe/layers/neuron_layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+template <typename Dtype>
+class DSPPLayer : public Layer<Dtype> {
+ public:
+  explicit DSPPLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual inline const char* type() const { return "DSPPLayer"; }
+  virtual inline int ExactNumBottomBlobs() const { return 2; };
+  virtual inline int MinTopBlobs() const { return 1; }
+ 
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  //virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  //    const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  //virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+  //    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  
+  int width_;
+  int height_;
+  int channel_;
+  int num_;
+
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_DSPP_LAYER_HPP_
